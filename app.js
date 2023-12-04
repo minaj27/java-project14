15b8145ecbd36a1a9583f09cd658a43f01ba97bf
minaj27 <minaj13m@gmail.com>
2023-12-04 23:56:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const emp1={
+    name: "Sharad",
+    city: "Pune",
+    empid: 25,
+    sal: 15000,
+}
+const emp2={
+    name: "Manoj",
+    city: "Bangalore",
+    empid: 55,
+    sal: 30000,
+}
+const emp3={
+    name: "Kavita",
+    city: "Pune",
+    empid:30,
+    sal: 20000,
+}
